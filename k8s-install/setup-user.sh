1e04747985427adf2adc7b1579e2a6947299ff38
Prempal Singh <prempal.singh@hcl.com>
2021-10-14 16:42:05 +0530

    update the user info

--- k8s-install/setup-user.sh
+++ k8s-install/setup-user.sh
@@ -105,9 +105,9 @@ update_conf()
 
 ############### MAIN ###################
 
-USER="devops"
-GROUP="devops"
-passw="today@1234"
+USER="sredemo"
+GROUP="sredemo"
+passw="sre@1234"
 
 if [ -f /etc/os-release ];then
    osname=`grep ID /etc/os-release | egrep -v 'VERSION|LIKE|VARIANT' | cut -d'=' -f2 | sed -e 's/"//' -e 's/"//'`
